2eeac38df69d8ee9c27cf781e22bfac25929345c
Michele Vascellari <michele.vascellari@gmail.com>
2020-03-24 20:16:07 +0000

    Use insert on key update

--- build.sbt
+++ build.sbt
@@ -4,13 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.12.10"
 
-
 libraryDependencies ++= Seq(
   // "org.apache.kafka" %% "kafka" % "2.1.0",
 //  "com.typesafe.play" %% "play-json" % "2.8.1",
- "org.scalikejdbc" %% "scalikejdbc"       % "3.4.0",
- "com.h2database"  %  "h2"                % "1.4.200",
- "ch.qos.logback"  %  "logback-classic"   % "1.2.3",
- "mysql" % "mysql-connector-java" % "8.0.19",
- "com.typesafe.play" %% "play-json" % "2.6.14"
+  "org.scalikejdbc" %% "scalikejdbc" % "3.4.0",
+  "com.h2database" % "h2" % "1.4.200",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "mysql" % "mysql-connector-java" % "8.0.19",
+  "com.typesafe.play" %% "play-json" % "2.6.14",
+  "org.scalactic" %% "scalactic" % "3.1.1",
+  "org.scalatest" %% "scalatest" % "3.1.1" % "test"
 )
--- src/main/scala/com/mvasce/Sql.scala
+++ src/main/scala/com/mvasce/Sql.scala
@@ -10,75 +10,81 @@ import play.api.libs.json.JsValue
 
 object Sql {
 
-    /**
-      * Get a map with the column names -> type of the given table
-      *
-      * @param tableName
-      * @return
-      */
-    def getColumns(tableName: String): Map[String, String] = {
-        // val columns = DB.getColumnNames(table)
-        val table: Table = DB.getTable(tableName).get
-        // define exception
-        table.columns.map(col => (col.name, col.typeName)).toMap // typeCode
-    }
+  /**
+    * Get a map with the column names -> type of the given table
+    *
+    * @param tableName
+    * @return
+    */
+  def getColumns(tableName: String): Map[String, String] = {
+    // val columns = DB.getColumnNames(table)
+    val table: Table = DB.getTable(tableName).get
+    // define exception
+    table.columns.map(col => (col.name, col.typeName)).toMap // typeCode
+  }
 }
 
 case class TableMachwell(name: String) {
-    val table: Table = DB.getTable(name).get
-    val columnsMap: Map[String, String] = table.columns.map(col => (col.name, col.typeName)).toMap
+  val table: Table = DB.getTable(name).get
+  val columnsMap: Map[String, String] =
+    table.columns.map(col => (col.name, col.typeName)).toMap
 
-    def castValue(column: String, value: JsValue): Any = {
-        columnsMap.get(column).get match  {
-            case "INT" => value.as[Int]
-            case "VARCHAR" => value.as[String]
-            case _ => null
-        }
+  def castValue(column: String, value: JsValue): Any = {
+    columnsMap.get(column).get match {
+      // TODO complete the list
+      case "INT"     => value.as[Int]
+      case "VARCHAR" => value.as[String]
+      case _         => null
     }
+  }
 
-    def insert(data: JsValue) = {
-        val object_ = data.as[JsObject].value
-        object_.keys.mkString(",")
-        val keyString = s"(${object_.keys.mkString(",")})"
-        val placeholderString = s"(${object_.map(x=>"?").mkString(", ")})"
-        println(placeholderString)
-        val cmd = s""" INSERT INTO $name 
+  def insert(data: JsValue) = {
+    val object_ = data.as[JsObject].value
+    object_.keys.mkString(",")
+    val keyString = s"(${object_.keys.mkString(",")})"
+    val placeholderString = s"(${object_.map(x => "?").mkString(", ")})"
+    val keyUpdate = object_.map(x=>s"${x._1}=?").mkString(",\n")
+    println(keyUpdate)
+    
+    println(placeholderString)
+    val cmd = s""" INSERT INTO $name 
             $keyString
             VALUES $placeholderString
+            ON DUPLICATE KEY UPDATE
+            $keyUpdate
             """.trim()
-        println(cmd)
-        val command = SQL(cmd)
-        val casted = object_.map {
-            x => castValue(x._1, x._2)
-        }.toList
-        command.bind(casted: _*).update     
-    }
+    println(cmd)
+    val command = SQL(cmd)
+    var casted = object_.map { x => castValue(x._1, x._2) }.toList
+    casted ++= casted
+    println(casted)
+    command.bind(casted: _*).update
+  }
 }
 
 object Run extends App {
-    val driver = "com.mysql.cj.jdbc.Driver"
-    Class.forName(driver)
-    val url = "jdbc:mysql://127.0.0.1/test"
-    val username = "root"
-    val password = "example"
-    ConnectionPool.singleton(url, username, password)
+  val driver = "com.mysql.cj.jdbc.Driver"
+  Class.forName(driver)
+  val url = "jdbc:mysql://127.0.0.1/test"
+  val username = "root"
+  val password = "example"
+  ConnectionPool.singleton(url, username, password)
 
-    val json = Json.parse("""{"age": 42, "name": "Michele"}""") 
-    val age = (json \ "age").get
+  val json = Json.parse("""{"user_id":11, "age": 42, "name": "Michele"}""")
+  val age = (json \ "age").get
 
-    // println(JDBCType.ARRAY)
+  // println(JDBCType.ARRAY)
 
-    DB autoCommit {
-        implicit session =>
-        // val columns = Sql.getColumns("users")
-        val table = TableMachwell("users")
-        // println(columns)
-        println(table)
+  DB autoCommit { implicit session =>
+    // val columns = Sql.getColumns("users")
+    val table = TableMachwell("users")
+    // println(columns)
+    println(table)
 
-        val x = table.castValue("age", age)
-        println(x)
+    val x = table.castValue("age", age)
+    println(x)
 
-        val command = table.insert(json)
-        command.apply()
-    }   
+    val command = table.insert(json)
+    command.apply()
+  }
 }
